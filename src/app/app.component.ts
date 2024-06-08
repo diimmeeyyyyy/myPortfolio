e04df08606b776514c2da239a32f08eaac43e9f9
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-08 17:04:30 +0200

    added about-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './above-the-fold/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboveTheFoldComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
