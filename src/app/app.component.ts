e805d60f6b0f39ffbc9ac60498a9bdae9ff0dcbf
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-10 10:56:33 +0200

    added skill-set section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './above-the-fold/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent],
+  imports: [
+    RouterOutlet,
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    SkillSetComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
