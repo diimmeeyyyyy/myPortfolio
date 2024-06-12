f5cbf952cb9ed0062cbb6e93aff63ae811308ce6
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-12 17:29:46 +0200

    implemented contact

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,6 +3,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
@@ -12,6 +13,7 @@ import { MyWorkComponent } from './my-work/my-work.component';
     SkillSetComponent,
     AboutMeComponent,
     MyWorkComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
