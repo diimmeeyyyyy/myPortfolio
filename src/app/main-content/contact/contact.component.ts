05b71427e3a3954318d93734ccdd66708e5e4820
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-12 20:09:58 +0200

    implemented contact.component + styled form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  checkboxChecked = false;
+  checkboxImg = '/assets/img/checkbox-unchecked-default.png';
 
+  changeCheckboxImg() {
+    if (!this.checkboxChecked) {
+      this.checkboxChecked = true;
+      this.checkboxImg = '/assets/img/checkbox-checked.png';
+    } else {
+      this.checkboxChecked = false;
+      this.checkboxImg = '/assets/img/checkbox-unchecked-default.png';
+    }
+  }
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  onSubmit(ngForm: NgForm) {
+    debugger;
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 }
