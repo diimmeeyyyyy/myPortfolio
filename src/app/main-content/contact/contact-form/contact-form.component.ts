b1bf280421e56616a5d7fbc68debfa9c73f0b63a
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-15 11:52:44 +0200

    optimized focus & blur of inputfields

--- src/app/main-content/contact/contact-form/contact-form.component.ts
+++ src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -10,23 +10,64 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
+  focusOnName = false;
+  focusOnEmail = false;
+  focusOnMessage = false;
   @ViewChild('Name_Input') nameField!: ElementRef;
   @ViewChild('Email_Input') emailField!: ElementRef;
-  focusInputField(inputfield: string) {
-    if (
-      inputfield === 'name' &&
-      this.nameField &&
-      this.nameField.nativeElement
-    ) {
-      this.nameField.nativeElement.focus();
+  @ViewChild('Message_Input') messageField!: ElementRef;
+
+  focusInputField(inputfield: 'name' | 'email' | 'message') {
+    const fields = {
+      name: this.nameField,
+      email: this.emailField,
+      message: this.messageField,
+    };
+    const field = fields[inputfield];
+    if (field && field.nativeElement) {
+      field.nativeElement.focus();
     }
-    if (
-      inputfield === 'email' &&
-      this.emailField &&
-      this.emailField.nativeElement
-    ) {
-      this.emailField.nativeElement.focus();
+    this.focusSettings(inputfield);
+  }
+
+  focusSettings(inputfield: string) {
+    switch (inputfield) {
+      case 'name':
+        this.focusOnMessage = false;
+        this.focusOnEmail = false;
+        this.focusOnName = true;
+        break;
+      case 'email':
+        this.focusOnMessage = false;
+        this.focusOnName = false;
+        this.focusOnEmail = true;
+        break;
+      case 'message':
+        this.focusOnEmail = false;
+        this.focusOnName = false;
+        this.focusOnMessage = true;
+        break;
     }
+  }
+
+  blurInputfield(inputfield: 'name' | 'email' | 'message') {
+    const fields = {
+      name: this.nameField,
+      email: this.emailField,
+      message: this.messageField,
+    };
+
+    const field = fields[inputfield];
+    if (field && field.nativeElement) {
+      field.nativeElement.blur();
+    }
+    this.blurSettings();
+  }
+
+  blurSettings() {
+    this.focusOnMessage = false;
+    this.focusOnEmail = false;
+    this.focusOnName = false;
   }
 
   checkboxChecked = false;
@@ -53,29 +94,5 @@ export class ContactFormComponent {
     if (ngForm.valid && ngForm.submitted) {
       console.log(this.contactData);
     }
-  }
-
-  isFocused = false;
-  onFocus() {
-    this.isFocused = true;
-  }
-  onBlur() {
-    this.isFocused = false;
-  }
-
-  focusOnName = false;
-  onFocusNameField() {
-    this.focusOnName = true;
-  }
-  onBlurNameField() {
-    this.focusOnName = false;
-  }
-
-  focusOnEmail = false;
-  onFocusEmailField() {
-    this.focusOnEmail = true;
-  }
-  onBlurEmailField() {
-    this.focusOnEmail = false;
   }
 }
