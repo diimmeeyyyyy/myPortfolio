df816a3a251947b8e50d9cedd166b6863e6e9f8c
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-19 11:30:32 +0200

    added media-queries for atf + about-me

--- src/app/main-content/above-the-fold/header/header.component.ts
+++ src/app/main-content/above-the-fold/header/header.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  hamburgerMenuOpen = false;
 
+  closeMenu() {
+    this.hamburgerMenuOpen = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  onCheckboxChange(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  screenIsLarge() {
+    return window.innerWidth > 1024;
+  }
 }
