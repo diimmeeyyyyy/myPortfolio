4aa8ab04e110870942c319b0a5772394dd44dc89
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-07-03 10:50:42 +0200

    optimized hover-effect

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -8,4 +8,17 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss',
 })
-export class AboveTheFoldComponent {}
+export class AboveTheFoldComponent {
+// In Ihrer Komponentenklasse
+hoverEffect(event: MouseEvent, entering: boolean) {
+  const target = event.target as HTMLElement;
+  if (target.tagName === 'IMG' && entering) {
+    target.style.transform = 'translateX(-15px)';
+    target.style.transition = 'transform 0.3s ease-in-out';
+  } else if (target.tagName === 'IMG') {
+    target.style.transition = 'transform 0.3s ease-in-out';
+    target.style.transform = 'translateX(0)';
+  }
+}
+
+}
